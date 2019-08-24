e9706a1d69a2614d10b0fd75997736b2e396ff77
Huang Jian-Min <yfr.huang@hotmail.com>
2019-08-24 22:21:34 +0800

    update build script again

--- run-local-build.sh
+++ run-local-build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+semanticVersion=0.0.1
+serial=$(date +"%Y%m%d%H%M")
+version=$semanticVersion.$serial
+
 ./gradlew sample-api-interface:clean
 ./gradlew sample-api:clean
 ./gradlew sample-task:clean
 
 ./gradlew sample-api:buildDocker -x test
 ./gradlew sample-task:buildDocker -x test
+
+docker tag jcconf-2019-sample/sample-api:0.0.1 jianminhuang/sample-api:$version
+docker tag jcconf-2019-sample/sample-task:0.0.1 jianminhuang/sample-task:$version
+
+docker push jianminhuang/sample-api:$version
+docker push jianminhuang/sample-task:$version
 
 docker rmi $(docker images -f "dangling=true" -q)
